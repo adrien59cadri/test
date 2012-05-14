00dfa513124d6e8143c29cf880335b70ef3b895b
adrien59cadri <adrien59cadri@gmail.com>
2012-05-15 00:16:39 +0200

    logon and logoff fb with python and http api

--- src/parse.py
+++ src/parse.py
@@ -20,17 +20,47 @@ res=urllib.request.urlopen(url)
 
 print (res.status, res.reason, res.msg)
 res_body=res.read()
-file=open("answer.xml","w")
 res_str=str(res_body,"iso-8859-1")
-file.write(res_str)
 
 
 print(res_str)
-import xml.etree 
-root=xml.etree.ElementTree.ElementTree(res_str)
-
+import xml.etree.ElementTree as ET
+root=ET.XML(res_str)
 element=root.find("error")
-if element :
-	print (element.name, element.tag, element.attribute)
-if element.tag is "error":
-	print("There is an error : "+element.text)
+if element is not None:
+	print (element.tag, element.attrib)
+	if element.tag == "error":
+		print("There is an error : "+element.text)
+		exit(1)#error bad login
+token = root.find("token").text
+
+print( token)
+
+
+
+print("\nlog off")
+command_="logoff"
+command = "cmd="+command_
+param1_=token
+param1="token="+param1_
+
+args=command+"&"+param1
+print ("request : "+address)
+print(args)
+url=address+args
+res=urllib.request.urlopen(url)
+
+print (res.status, res.reason, res.msg)
+res_body=res.read()
+res_str=str(res_body,"iso-8859-1")
+
+
+print(res_str)
+import xml.etree.ElementTree as ET
+root=ET.XML(res_str)
+element=root.find("error")
+if element is not None:
+	print (element.tag, element.attrib)
+	if element.tag == "error":
+		print("There is an error : "+element.text)
+		exit(1)#error bad logoff
