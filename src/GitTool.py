4bf4032d1a1c8a80c50a241ca668424d4802b2f2
acourdavault <adrien.courdavault@arturia.com>
2012-05-15 16:15:42 +0200

    et voila!

--- src/GitTool.py
+++ src/GitTool.py
@@ -1,14 +1,18 @@
 import xml.etree.ElementTree as ET
-import os
+import subprocess
 #bash command
-command= "git log --pretty=format:'<commit>%n<ref>%h</ref>%n<author>%an</author>%n<date>%ai</date>%n<message>%s</message>%n</commit>' 8409f6f..6f4677f > log.xml"
+#command= "git log --pretty=format:'<commit>%n<ref>%h</ref>%n<author>%an</author>%n<date>%ai</date>%n<message>%s</message>%n</commit>' 8409f6f..6f4677f > log.xml"
+old="8409f6f"
+new="6f4677f"
+format="--pretty=format:'<commit>%n<ref>%h</ref>%n<author>%an</author>%n<date>%ai</date>%n<message>%s</message>%n</commit>"
 
-
-os.system(command)
+print("RUN COMMAND")
+subprocess.Popen(['git', 'log', format, old+".."+new], stdout=subprocess.PIPE)
+output = git.stdout.read()
 
 
 print ("XML")
-file=open("log.xml")
-root=ET.XML(file.read())
-commits=root.findAll("commit")
-print (commits)
+# file=open("log.xml")
+# root=ET.XML(file.read())
+# commits=root.findAll("commit")
+# print (commits)
