7b6487eab60bc9de8ff66c52ec74c9db1d9b78b1
acourdavault <adrien.courdavault@arturia.com>
2012-05-15 15:32:35 +0200

    modifs

--- src/GitTool.py
+++ src/GitTool.py
@@ -1,4 +1,16 @@
-
-
+import xml.etree.ElementTree as ET
+import subprocess
 #bash command
-#$ git log --pretty=format:"<commit>%n<ref>%h</ref>%n<author>%an</author>%n<date>%ai</date>%n<message>\"%s\"</message>%n</commit>" 8409f6f..6f4677f  >log.xml
+command= "git log --pretty=format:\"<commit>%n<ref>%h</ref>%n<author>%an</author>%n<date>%ai</date>%n<message>%s</message>%n</commit>\" 8409f6f..6f4677f"
+
+
+p = subprocess.Popen(["ls"], stdout=subprocess.PIPE)
+res_str = p.communicate()[0]
+print (res_str)
+
+# print ("XML")
+# root=ET.XML(res_str)
+
+# commits=root.findAll("commit")
+
+# print (commits)
