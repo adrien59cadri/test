78adf910a9933c7d7999f079f66fb11d71b0a00c
Dzada <dzada@dzada-VirtualBox.(none)>
2012-05-15 15:56:51 +0200

    voici les modifs

--- src/GitTool.py
+++ src/GitTool.py
@@ -1,16 +1,14 @@
 import xml.etree.ElementTree as ET
-import subprocess
+import os
 #bash command
-command= "git log --pretty=format:\"<commit>%n<ref>%h</ref>%n<author>%an</author>%n<date>%ai</date>%n<message>%s</message>%n</commit>\" 8409f6f..6f4677f"
+command= "git log --pretty=format:'<commit>%n<ref>%h</ref>%n<author>%an</author>%n<date>%ai</date>%n<message>%s</message>%n</commit>' 8409f6f..6f4677f > log.xml"
 
 
-p = subprocess.Popen(["ls"], stdout=subprocess.PIPE)
-res_str = p.communicate()[0]
-print (res_str)
+os.system(command)
 
-# print ("XML")
-# root=ET.XML(res_str)
 
-# commits=root.findAll("commit")
-
-# print (commits)
+print ("XML")
+file=open("log.xml")
+root=ET.XML(file.read())
+commits=root.findAll("commit")
+print (commits)
