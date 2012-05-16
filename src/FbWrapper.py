da08a1de878ac91f7c5c13ca6232c77a7efd13f0
acourdavault <adrien.courdavault@arturia.com>
2012-05-16 10:11:08 +0200

    fix sur la classe fb, et code de base dans gittool pour gnrer une request fb checkin; Cependant ne marche pas pour des raison inconnues (mauvise info ??, ou mauvais rglage sur FB

--- src/FbWrapper.py
+++ src/FbWrapper.py
@@ -5,15 +5,16 @@ import xml.etree.ElementTree as ET
 class FbWrapper:
 	def __init__(self):
 		self.address="https://dzada.fogbugz.com/api.asp?"
+		self.logedon=False
 		
-	def doRequest(url):
+	def doRequest(self,url):
 		res=urllib.request.urlopen(url)
 		print (res.status, res.reason, res.msg)
 		res_body=res.read()
 		res_str=str(res_body,"iso-8859-1")
 		return res_str
 	
-	def checkError(res_str):
+	def checkError(self,res_str):
 		print(res_str)
 		root=ET.XML(res_str)
 		element=root.find("error")
@@ -25,37 +26,45 @@ class FbWrapper:
 				exit(1)#error
 		return root
 				
-	def createUrl(cmd,params):
+	def createUrl(self,cmd,params):
+		tokens=""
+		if self.logedon:
+			tokens="token="+self.token+"&"
 		args="cmd="+cmd
 		for p in params:
-			args+=+"&"+params[0]+"="+params[1]
+			args+="&"+p[0]+"="+p[1]
 		print ("request : "+self.address)
-		print(args)
-		url=self.address+args
+		print(tokens+args)
+		url=self.address+tokens+args
 		return url
 		
-	def logon(user,pwd):
-		url=self.createUrl("logon",[["email",email],["password",pwd]])
+	def logon(self,user,pwd):
+		print("LOGON")
+		url=self.createUrl("logon",[["email",user],["password",pwd]])
 		out=self.doRequest(url)
 		xml=self.checkError(out)
 		##now real thing
 		self.token=xml.find("token").text
 		print(self.token)
+		self.logedon=True
 	
-	def logoff():
+	def logoff(self):
+		print("LOGOFF")
 		url=self.createUrl("logoff",[["token",self.token]])
 		out=self.doRequest(url)
 		xml=self.checkError(out)
+		self.logedon=False
 		##now real thing
 		#ras
 		
-	def newCheckin(bugid, files, revisions,repo):
+	def newCheckin(self,bugid, files, revisions,repo):
+		print("NEW_CHECKIN")
 		cmd="newCheckin"
 		url=self.createUrl(cmd,[["ixBug",bugid],["sFile",files],["sPrev",revisions[0]],["sNext",revisions[1]],["ixRepository",repo]])
 		out=self.doRequest(url)
 		xml=self.checkError(out)
 
-	def listCheckin(bugid):
+	def listCheckin(self,bugid):
 		url=self.createUrl(cmd,[["ixBug",bugid]])
 		out=self.doRequest(url)
 		xml=self.checkError(out)
--- src/GitTool.py
+++ src/GitTool.py
@@ -1,5 +1,7 @@
 import xml.etree.ElementTree as ET
 import subprocess
+import FbWrapper
+
 #bash command
 #command= "git log --pretty=format:'<commit>%n<ref>%h</ref>%n<author>%an</author>%n<date>%ai</date>%n<message>%s</message>%n</commit>' 8409f6f..6f4677f > log.xml"
 old="8409f6f"
@@ -11,6 +13,8 @@ subp=subprocess.Popen(['git', 'log', format, old+".."+new], stdout=subprocess.PI
 outputb=subp.stdout.read()
 output = "<list>\n"+str(outputb,"iso-8859-1")+"\n</list>"
 
+fb = FbWrapper.FbWrapper()
+fb.logon('courdava@enserg.fr','p455w04d')
 
 print ("XML")
 root=ET.XML(output)
@@ -22,5 +26,15 @@ for commit in commits:
 	message=commit.find("message").text
 	reference=commit.find("ref").text
 	print("\nCommit : "+reference+"\nby "+authorname+" ["+date+"]\n"+message)
+	
+	print("Request to fogbugz generation")
+	
+	
+	my_repo_name = "my_repo"
+	repo_id = "1" #saw this in fb
+	bug_id = "2"
+	files = "file"#date +" "+ authorname#we lie
+	fb.newCheckin(bug_id,files,[old,new],repo_id)
 
+fb.logoff()
 
