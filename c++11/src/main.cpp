9df57d07f611d6873810b6b100a21b57b1f71842
acourdavault <adrien.courdavault@arturia.com>
2013-12-03 19:47:43 +0100

    code to test move

--- c++11/src/main.cpp
+++ c++11/src/main.cpp
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 
-int main(int argc, char * argv[]){
-	return 0;
+using namespace std;
+struct A{
+    A(){ cout << "ctor A\n"; }
+    A(const A&){ cout << "cpy A\n"; }
+    A(A&&){ cout << "move A\n"; }
+    A& operator= (A&&) { cout << "move_assign A\n"; return *this; }
+    A& operator= (const A&) { cout << "cpy_assign A\n"; return *this; }
+
+    int t;
+};
+
+A create(){ return A{}; }
+
+void test_move(){
+
+    cout << "<<<\n";
+    auto v = create();
+    A v1{ create() };
+    cout << "<<<\n";
+
+    A v2 = v;
+
+    A v3{ v };
+
+
+    cout << "<<<\n";
+    auto v_ = std::move(create());
+    cout << "<<<\n";
+    v = v_;
+    cout << "<<<\n";
+    v_ = std::move(v);
+    cout << "<<<\n";
+
+}
+
+int main(int argc, char * argv []){
+    test_move();
+
+    return 0;
 }
